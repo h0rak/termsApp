3707a53f81919d49611b971b2cc7c01fa5d9d03a
h0rak <trey.horak@gmail.com>
2023-01-12 07:37:02 -0600

    tenth commit saving before going to work

--- termsApp/app/src/main/java/com/example/termsapp/UI/AssessmentDetail.java
+++ termsApp/app/src/main/java/com/example/termsapp/UI/AssessmentDetail.java
@@ -3,14 +3,47 @@ package com.example.termsapp.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.termsapp.Database.Repository;
 import com.example.termsapp.R;
 
 public class AssessmentDetail extends AppCompatActivity {
+
+    int aID;
+    String aType;
+    String aStart;
+    String aEnd;
+    int cID;
+    Repository repository;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_assessmentdetail, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+            case R.id.saveAssessment:
+                // save
+
+            case R.id.deleteAssessment:
+                // delete
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
